516c2e571a80b1224464ad3f91277760dd74f88c
Yusuf İtmiş <yusufitms@gmail.com>
2024-10-20 07:37:45 +0300

    Create oklid.py

---
+++ oklid.py
@@ -0,0 +1,15 @@
+def euclideanDistance(point1, point2):
+    return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
+
+points = [(1, 2), (4, 6), (5, 8), (3, 1), (7, 2)]
+
+distances = []
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+
+print("Minimum mesafe:", min_distance)
